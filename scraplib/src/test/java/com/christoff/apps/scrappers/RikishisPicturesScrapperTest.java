21aaa832144bcae721688e598d892a00d9b31751
Christ-OFF <4630125+Christ-OFF@users.noreply.github.com>
2017-09-19 22:57:47 +0200

    FIX #3 - Remove @Rule (junit4)

--- scraplib/src/test/java/com/christoff/apps/scrappers/RikishisPicturesScrapperTest.java
+++ scraplib/src/test/java/com/christoff/apps/scrappers/RikishisPicturesScrapperTest.java
@@ -1,11 +1,12 @@
 package com.christoff.apps.scrappers;
 
 import com.christoff.apps.sumo.lambda.domain.RikishiPicture;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.google.common.io.Resources;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import ru.lanwen.wiremock.ext.WiremockResolver;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,34 +18,34 @@ import static org.junit.Assert.*;
 /**
  * Created by christophe on 30.04.17.
  */
+@ExtendWith({
+    WiremockResolver.class
+})
 public class RikishisPicturesScrapperTest {
 
     /**
      * for the test I have ONE picture, could have more...
      */
     private static final String RIKISHI_PICTURE = "42.jpg";
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule();
-    private RikishiPicturesScrapper tested;
 
-    @Before
-    public void setup() {
-        RikishisPicturesScrapParameters params = new RikishisPicturesScrapParameters.Builder().withBaseUrl("http://localhost:8080/").build();
-        tested = new RikishiPicturesScrapper(params);
-    }
-
-    @Test(expected = UnsupportedOperationException.class)
+    @Test
     public void should_not_provide_select_method() {
-        tested.select();
+        RikishisPicturesScrapParameters params = new RikishisPicturesScrapParameters.Builder().build();
+        RikishiPicturesScrapper tested = new RikishiPicturesScrapper(params);
+        Assertions.assertThrows(UnsupportedOperationException.class, tested::select);
     }
 
     @Test
-    public void should_scrap_picture() throws IOException {
+    public void should_scrap_picture(@WiremockResolver.Wiremock WireMockServer server) throws IOException {
         // Given
+        RikishisPicturesScrapParameters params = new RikishisPicturesScrapParameters.Builder()
+            .withBaseUrl("http://localhost:" + server.port() + "/")
+            .build();
+        RikishiPicturesScrapper tested = new RikishiPicturesScrapper(params);
         int expectedId = 42;
         URL urlPicture = Resources.getResource("42.jpg");
         byte[] bodyPicture = Resources.toByteArray(urlPicture);
-        stubFor(get(urlEqualTo("/" + RikishisPicturesScrapParameters.DEFAULT_PICS_PATH + "42.jpg"))
+        server.stubFor(get(urlEqualTo("/" + RikishisPicturesScrapParameters.DEFAULT_PICS_PATH + "42.jpg"))
             .willReturn(aResponse()
                 .withStatus(200)
                 .withBody(bodyPicture)));
@@ -57,9 +58,14 @@ public class RikishisPicturesScrapperTest {
     }
 
     @Test
-    public void should_return_null_on_missing_image() throws IOException, ParseException {
+    public void should_return_null_on_missing_image(@WiremockResolver.Wiremock WireMockServer server) throws IOException, ParseException {
         // Given
-        stubFor(get(urlEqualTo("/" + RikishisPicturesScrapParameters.DEFAULT_PICS_PATH + "666.jpg")).willReturn(aResponse().withStatus(404)));
+        RikishisPicturesScrapParameters params = new RikishisPicturesScrapParameters.Builder()
+            .withBaseUrl("http://localhost:" + server.port() + "/")
+            .build();
+        RikishiPicturesScrapper tested = new RikishiPicturesScrapper(params);
+        server.stubFor(get(urlEqualTo("/" + RikishisPicturesScrapParameters.DEFAULT_PICS_PATH + "666.jpg"))
+            .willReturn(aResponse().withStatus(404)));
         // When
         RikishiPicture result = tested.getDetail(666);
         // Then
