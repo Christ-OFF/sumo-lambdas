1a2e727ef657a732b50f18622035145750ffec86
Christ-OFF <4630125+Christ-OFF@users.noreply.github.com>
2017-10-05 23:52:17 +0200

    FIX #6- Reduce image size using java.nio

--- lambda-rikishi-picture-scrap/src/main/java/com/christoff/apps/sumolambda/RikishiPictureScrapperService.java
+++ lambda-rikishi-picture-scrap/src/main/java/com/christoff/apps/sumolambda/RikishiPictureScrapperService.java
@@ -12,10 +12,16 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.*;
+import javax.imageio.stream.ImageOutputStream;
+import javax.imageio.stream.MemoryCacheImageInputStream;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.util.Iterator;
 
 /**
  * The service called from the handler to process rikishis
@@ -28,7 +34,17 @@ public class RikishiPictureScrapperService {
     private static final Logger LOGGER = Logger.getLogger(RikishiPictureScrapperService.class);
 
     private static final String DEFAULT_JPG = "default.jpg";
-    public static final String CONTENT_TYPE = "image/jpeg";
+    private static final String CONTENT_TYPE = "image/jpeg";
+
+    /**
+     * We will decrease jpeg image quality to this factor
+     */
+    private static final Float QUALITY = 0.3F;
+
+    /**
+     * Applied on this format
+     */
+    private static final String IMAGE_FORMAT = "jpg";
 
     private final
     AmazonS3 s3;
@@ -70,25 +86,73 @@ public class RikishiPictureScrapperService {
     }
 
     /**
+     * This is the method to decrease image quality
+     */
+    private byte[] reducePicture(byte[] picture) throws IOException {
+        // First step "read" the JPG in memory
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(picture)) {
+            ImageReader imageReader = getImageReader(bais);
+            // Second Get writers
+            Iterator<ImageWriter> writerIterator = ImageIO.getImageWritersByFormatName(IMAGE_FORMAT);
+            ImageWriter imageWriter = writerIterator.next();
+            ImageWriteParam imageWriteParam = getImageWriteParam(imageWriter);
+            // Prepare output
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageOutputStream imageOutputStream = new MemoryCacheImageOutputStream(baos);
+            imageWriter.setOutput(imageOutputStream);
+            IIOImage iioimage = new IIOImage(imageReader.read(0), null, null);
+            // Write and flush
+            imageWriter.write(null, iioimage, imageWriteParam);
+            imageOutputStream.flush();
+            return baos.toByteArray();
+        }
+
+    }
+
+    /**
+     * @param imageWriter the source writer
+     * @return the paramters to apply
+     */
+    @NotNull
+    private ImageWriteParam getImageWriteParam(ImageWriter imageWriter) {
+        ImageWriteParam imageWriteParam = imageWriter.getDefaultWriteParam();
+        imageWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        imageWriteParam.setCompressionQuality(QUALITY);
+        return imageWriteParam;
+    }
+
+    /**
+     * Return an ImageReader for JPG
+     *
+     * @param bais the inputstream of the jpg to read
+     * @return an JPEG Image reader
+     */
+    @NotNull
+    private ImageReader getImageReader(ByteArrayInputStream bais) {
+        MemoryCacheImageInputStream cache = new MemoryCacheImageInputStream(bais);
+        Iterator<ImageReader> readerIterator = ImageIO.getImageReadersByFormatName(IMAGE_FORMAT);
+        ImageReader imageReader = readerIterator.next();
+        imageReader.setInput(cache);
+        return imageReader;
+    }
+
+    /**
      * This method purpose is to store the rikishi picture asIs in S3
      *
-     * @param rikishiId
-     * @param result
+     * @param rikishiId the Id of the rikishi to compute image name
      */
     private void storePictureToS3(int rikishiId, RikishiPicture result) {
-        // Storing to S3
-        // First Input Stream
-        try (ByteArrayInputStream bais = new ByteArrayInputStream(result.getPicture().array())) {
-
+        try {
+            byte[] reduced = reducePicture(result.getPicture().array());
             // Second metadata with InputStream
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentType(CONTENT_TYPE);
-            metadata.setContentLength(result.getPicture().array().length);
+            metadata.setContentLength(reduced.length);
             s3.putObject(params.getBucket(),
                 rikishiId + ".jpg",
-                bais,
+                new ByteArrayInputStream(reduced),
                 metadata);
-            LOGGER.info("Saved " + rikishiId + ".jpg to " + params.getBucket());
+            LOGGER.info("Saved " + rikishiId + ".jpg to " + params.getBucket() + "with size " + reduced.length);
         } catch (IOException ioe) {
             LOGGER.error("Unable to read bytes from image. Abort S3Store of " + rikishiId);
         }
