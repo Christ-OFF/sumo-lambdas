b25a640af3df31923f9d46457f15b1517fe01da1
Christ-OFF <4630125+Christ-OFF@users.noreply.github.com>
2017-10-08 16:51:59 +0200

    CHANGE - Removed ExtractInfoGet java as NodeJS performs way better

--- lambda-extractinfo-get/src/main/java/com/christoff/apps/sumolambda/ExtractInfoRequest.java
+++
@@ -1,9 +0,0 @@
-package com.christoff.apps.sumolambda;
-
-/**
- * We have no parameter (for now) so this class is empty
- * Created by christophe on 19.06.17.
- */
-public class ExtractInfoRequest {
-
-}
